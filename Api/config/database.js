c23a8a7cc13803793a17f374ffdc20bffeabb61f
Vytenis domasevicius <vytenis.domasevicius@vka.lt>
2024-09-23 19:39:59 +0300

    db config changes

--- Api/config/database.js
+++ Api/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('database_development', 'postgres', 'mopsas555', {
-  host: 'localhost', // Database host
+const sequelize = new Sequelize('database_development_2mqs', 'vytenis', 'm0Nmsxw18lnUBztaugbpKmkcHygFPckL', {
+  host: 'dpg-cropi4i3esus73c5jlmg-a', // Database host
   dialect: 'postgres', // Database type (e.g., 'mysql', 'postgres')
 });
 
