f9003d08e7ecf08e7002017339dad371f94027bb
Vytenis domasevicius <vytenis.domasevicius@vka.lt>
2024-10-28 16:48:21 +0200

    port issues

--- Api/config/database.js
+++ Api/config/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('railway', 'postgres', 'tJewwQPAcVsGdUgYskBqanhhySzmLJMx', {
-  host: 'junction.proxy.rlwy.net:31805', // Database host
+  host: 'junction.proxy.rlwy.net', // Database host
+  port: '31805',
   dialect: 'postgres', // Database type (e.g., 'mysql', 'postgres')
 });
 
